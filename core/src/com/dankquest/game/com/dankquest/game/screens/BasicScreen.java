b16bc3f944a330098ef0db329571ab77fc91f474
Mikolaj <mikolaj.nalecz@hotmail.com>
2015-08-26 21:03:51 +0200

    Base classes added.

---
+++ core/src/com/dankquest/game/com/dankquest/game/screens/BasicScreen.java
@@ -0,0 +1,37 @@
+package com.dankquest.game.com.dankquest.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+
+public abstract class BasicScreen implements Screen {
+    Game game;
+
+    public BasicScreen (Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void resize (int width, int height) {
+    }
+
+    @Override
+    public void show () {
+    }
+
+    @Override
+    public void hide () {
+    }
+
+    @Override
+    public void pause () {
+    }
+
+    @Override
+    public void resume () {
+    }
+
+    @Override
+    public void dispose () {
+    }
+}
---
+++ core/src/com/dankquest/game/com/dankquest/game/screens/MainMenu.java
@@ -0,0 +1,28 @@
+package com.dankquest.game.com.dankquest.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class MainMenu extends BasicScreen {
+
+    public MainMenu(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void hide() {
+        //Pamietamy ¿e na wszystkimi trzeba wywolywac .dispose()
+    }
+}
