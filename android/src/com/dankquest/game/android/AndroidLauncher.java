2240ca85c3ed8179c3796a43e9f881e843f3cc70
Mikolaj <mikolaj.nalecz@hotmail.com>
2015-08-26 20:29:27 +0200

    Initial commit.

---
+++ android/src/com/dankquest/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dankquest.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dankquest.game.GameCore;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameCore(), config);
+	}
+}
---
+++ desktop/src/com/dankquest/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dankquest.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dankquest.game.GameCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameCore(), config);
+	}
+}
