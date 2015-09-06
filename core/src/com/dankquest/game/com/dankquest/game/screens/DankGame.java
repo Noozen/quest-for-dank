6e31871d60d5a8ec46bf946514c86d798cf7d978
Antah <KTamowicz@gmail.com>
2015-09-06 18:48:40 +0200

    Added target cancelling
    Also made some refactoring in DankGame

--- core/src/com/dankquest/game/com/dankquest/game/screens/DankGame.java
+++ core/src/com/dankquest/game/com/dankquest/game/screens/DankGame.java
@@ -24,9 +24,6 @@ import com.dankquest.game.com.dankquest.game.logic.Hero;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import java.awt.*;
 import java.util.Comparator;
 
 /**
@@ -35,23 +32,22 @@ import java.util.Comparator;
 public class DankGame extends BasicScreen {
     private Stage stage;
     private Table table;
-
+    //Our beautiful assets
     private Music battleMusic;
-
-    private Image backgroundImage, backgroundImage1;
-
+    private Image backgroundFloorImage, backgroundWallImage;
+    //UI
     private Skin skin;
-
     private TextButton retreatButton, castButton;
-
     private PortraitActor portraitActor;
-
     private SkillActor skill1, skill2, skill3, skill4;
+    //Heroes
     private HeroActor hero1, hero2, hero3, hero4;
     private HeroActor enemy1, enemy2, enemy3, enemy4;
-    private int skillCast;
-    private boolean targetMode;
+    //Targeting
+    private int skillCast = 0;
+    private boolean targetMode = false;
     private List<Hero> targetList = new ArrayList<Hero>();
+
 
     public DankGame(Game game) {
         super(game);
@@ -60,7 +56,15 @@ public class DankGame extends BasicScreen {
 
     @Override
     public void show() {
-        //Lista postaci
+        HeroesListSetup();
+        StageSetup();
+        MusicSetup();
+        BackgroundSetup();
+        UISetup();
+        IfComputerBegins();
+    }
+
+    private void HeroesListSetup() {
         Dank.allHeroesInGameList.addAll(Dank.chosenHeroesList);
         Dank.allHeroesInGameList.addAll(Dank.enemyHeroesList);
         Dank.allHeroesInGameList.sort((new Comparator<Hero>() {
@@ -77,33 +81,48 @@ public class DankGame extends BasicScreen {
             }
         }));
         Dank.thisTurnLeftHeroesList.addAll(Dank.allHeroesInGameList);
-
-        //Setup stag'a
-        stage = new Stage(new FitViewport(640,480));
+        Dank.activeHero = Dank.allHeroesInGameList.get(0);
+    }
+    private void StageSetup() {
+        stage = new Stage(new FitViewport(640, 480));
         Gdx.input.setInputProcessor(stage);
 
         table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
-        //Music Setup
+    }
+    private void MusicSetup() {
         battleMusic = Gdx.audio.newMusic(Gdx.files.internal("music/Battle2.ogg"));
         battleMusic.setVolume(0.2f);
         battleMusic.setLooping(true);
         battleMusic.play();
-
+    }
+    private void BackgroundSetup() {
         //Background Setup
-        backgroundImage = new Image(new TextureRegion(new Texture(Gdx.files.internal("backgrounds/Castle.png"))));
-        backgroundImage1 = new Image(new TextureRegion(new Texture(Gdx.files.internal("backgrounds/Castle1.png"))));
-        table.addActor(backgroundImage);
-        table.addActor(backgroundImage1);
+        backgroundFloorImage = new Image(new TextureRegion(new Texture(Gdx.files.internal("backgrounds/Castle.png"))));
+        backgroundWallImage = new Image(new TextureRegion(new Texture(Gdx.files.internal("backgrounds/Castle1.png"))));
+        table.addActor(backgroundFloorImage);
+        table.addActor(backgroundWallImage);
+    }
 
+    private void UISetup() {
         //Skin setup
         skin = new Skin(Gdx.files.internal("skins/rainbowpack.json"),
                 new TextureAtlas(Gdx.files.internal("skins/rainbowpack.pack")));
 
-        //Retreat Button Setup
-        retreatButton = new TextButton(":<", skin, "orange_yellow_fat");
+        RetreatButtonSetup();
+        CastButtonSetup();
+        SkillsSetup();
+        HeroesSetup();
+        EnemiesSetup();
+
+        //Portrait Actor
+        portraitActor = new PortraitActor();
+        table.addActor(portraitActor);
+    }
+    private void RetreatButtonSetup() {
+        retreatButton = new TextButton("Babyrage", skin, "orange_yellow_fat");
 
         retreatButton.setWidth(50);
         retreatButton.setHeight(50);
@@ -124,15 +143,57 @@ public class DankGame extends BasicScreen {
         });
 
         table.addActor(retreatButton);
+    }
+    private void CastButtonSetup() {
+        castButton = new TextButton("Cast", skin, "orange_yellow_fat");
 
-        //Active hero
-        Dank.activeHero = Dank.allHeroesInGameList.get(0);
+        castButton.setWidth(100);
+        castButton.setHeight(100);
 
-        //Skill buttons setup
-        skill1 = new SkillActor(200,10,1);
-        skill2 = new SkillActor(260,10,2);
-        skill3 = new SkillActor(320,10,3);
-        skill4 = new SkillActor(380,10,4);
+        castButton.setX(530);
+        castButton.setY(10);
+
+        castButton.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                if (targetMode == false)
+                    return;
+                if (Dank.chosenHeroesList.contains(Dank.activeHero)) {
+                    Dank.activeHero.getSkill(skillCast).cast(targetList);
+                    targetMode = false;
+                    checkIfGameEnded();
+                    clearTargetsAndSortHeroList();
+                }
+                while (Dank.enemyHeroesList.contains(Dank.activeHero)) {
+                    artificialIntelligence();
+                    checkIfGameEnded();
+                    clearTargetsAndSortHeroList();
+                }
+                hero1.update();
+                hero2.update();
+                hero3.update();
+                hero4.update();
+                enemy1.update();
+                enemy2.update();
+                enemy3.update();
+                enemy4.update();
+                skill1.update();
+                skill2.update();
+                skill3.update();
+                skill4.update();
+                portraitActor.update();
+            }
+        });
+        table.addActor(castButton);
+    }
+    private void SkillsSetup() {
+        skill1 = new SkillActor(200, 10, 1);
+        skill2 = new SkillActor(260, 10, 2);
+        skill3 = new SkillActor(320, 10, 3);
+        skill4 = new SkillActor(380, 10, 4);
         skill1.addListener(new InputListener() {
 
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -140,8 +201,13 @@ public class DankGame extends BasicScreen {
             }
 
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if (targetMode == true)
+                if (targetMode == true) {
+                    if (skillCast == 1) {
+                        targetMode = false;
+                        targetList.clear();
+                    }
                     return;
+                }
                 skillCast = 1;
                 targetMode = true;
                 targetList.add(Dank.activeHero);
@@ -154,8 +220,13 @@ public class DankGame extends BasicScreen {
             }
 
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if (targetMode == true)
+                if (targetMode == true) {
+                    if (skillCast == 2) {
+                        targetMode = false;
+                        targetList.clear();
+                    }
                     return;
+                }
                 skillCast = 2;
                 targetMode = true;
                 targetList.add(Dank.activeHero);
@@ -168,8 +239,13 @@ public class DankGame extends BasicScreen {
             }
 
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if (targetMode == true)
+                if (targetMode == true) {
+                    if (skillCast == 3) {
+                        targetMode = false;
+                        targetList.clear();
+                    }
                     return;
+                }
                 skillCast = 3;
                 targetMode = true;
                 targetList.add(Dank.activeHero);
@@ -182,8 +258,13 @@ public class DankGame extends BasicScreen {
             }
 
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if (targetMode == true)
+                if (targetMode == true) {
+                    if (skillCast == 4) {
+                        targetMode = false;
+                        targetList.clear();
+                    }
                     return;
+                }
                 skillCast = 4;
                 targetMode = true;
                 targetList.add(Dank.activeHero);
@@ -193,12 +274,12 @@ public class DankGame extends BasicScreen {
         table.addActor(skill2);
         table.addActor(skill3);
         table.addActor(skill4);
-
-        //Heroes setup
-        hero1 = new HeroActor(10,120,1, true);
-        hero2 = new HeroActor(60,180,2, true);
-        hero3 = new HeroActor(110,240,3, true);
-        hero4 = new HeroActor(160,300,4, true);
+    }
+    private void HeroesSetup() {
+        hero1 = new HeroActor(10, 120, 1, true);
+        hero2 = new HeroActor(60, 180, 2, true);
+        hero3 = new HeroActor(110, 240, 3, true);
+        hero4 = new HeroActor(160, 300, 4, true);
         hero1.addListener(new InputListener() {
 
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -251,12 +332,12 @@ public class DankGame extends BasicScreen {
         table.addActor(hero3);
         table.addActor(hero2);
         table.addActor(hero1);
-
-        //Enemy setup
-        enemy1 = new HeroActor(566,120,1, false);
-        enemy2 = new HeroActor(516,180,2, false);
-        enemy3 = new HeroActor(466,240,3, false);
-        enemy4 = new HeroActor(416,300,4, false);
+    }
+    private void EnemiesSetup() {
+        enemy1 = new HeroActor(566, 120, 1, false);
+        enemy2 = new HeroActor(516, 180, 2, false);
+        enemy3 = new HeroActor(466, 240, 3, false);
+        enemy4 = new HeroActor(416, 300, 4, false);
         enemy1.addListener(new InputListener() {
 
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -309,57 +390,9 @@ public class DankGame extends BasicScreen {
         table.addActor(enemy3);
         table.addActor(enemy2);
         table.addActor(enemy1);
+    }
 
-        //Portrait Actor
-        portraitActor = new PortraitActor();
-        table.addActor(portraitActor);
-
-        //Cast Button Setup
-        castButton = new TextButton("Cast", skin, "orange_yellow_fat");
-
-        castButton.setWidth(100);
-        castButton.setHeight(100);
-
-        castButton.setX(530);
-        castButton.setY(10);
-
-        castButton.addListener(new InputListener() {
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if (targetMode == false)
-                    return;
-                if (Dank.chosenHeroesList.contains(Dank.activeHero)) {
-                    Dank.activeHero.getSkill(skillCast).cast(targetList);
-                    targetMode = false;
-                    checkIfGameEnded();
-                    clearTargetsAndSortHeroList();
-                }
-                while (Dank.enemyHeroesList.contains(Dank.activeHero)) {
-                    artificialIntelligence();
-                    checkIfGameEnded();
-                    clearTargetsAndSortHeroList();
-                }
-                hero1.update();
-                hero2.update();
-                hero3.update();
-                hero4.update();
-                enemy1.update();
-                enemy2.update();
-                enemy3.update();
-                enemy4.update();
-                skill1.update();
-                skill2.update();
-                skill3.update();
-                skill4.update();
-                portraitActor.update();
-            }
-        });
-        table.addActor(castButton);
-
-
+    private void IfComputerBegins() {
         //If computer begins
         while (Dank.enemyHeroesList.contains(Dank.activeHero)) {
             artificialIntelligence();
@@ -379,11 +412,11 @@ public class DankGame extends BasicScreen {
         skill3.update();
         skill4.update();
         portraitActor.update();
-
     }
 
+
     private void resetHealthAndStuff() {
-        for(Hero hero : Dank.allHeroesInGameList) {
+        for (Hero hero : Dank.allHeroesInGameList) {
             hero.healthCurrent = hero.healthTotal;
         }
     }
@@ -391,18 +424,18 @@ public class DankGame extends BasicScreen {
     private void checkIfGameEnded() {
         boolean win = true;
         boolean lose = true;
-        for(Hero hero : Dank.enemyHeroesList){
-            if(hero.healthCurrent>0) {
+        for (Hero hero : Dank.enemyHeroesList) {
+            if (hero.healthCurrent > 0) {
                 win = false;
             }
         }
-        for(Hero hero : Dank.chosenHeroesList){
-            if(hero.healthCurrent>0) {
+        for (Hero hero : Dank.chosenHeroesList) {
+            if (hero.healthCurrent > 0) {
                 lose = false;
             }
         }
-        if(win) {
-            for(Hero hero : Dank.allHeroesInGameList) {
+        if (win) {
+            for (Hero hero : Dank.allHeroesInGameList) {
                 hero.healthCurrent = hero.healthTotal;
             }
             Image winImage = new Image(new Texture(Gdx.files.internal("inne/victory.png")));
@@ -411,8 +444,8 @@ public class DankGame extends BasicScreen {
             table.addActor(winImage);
             resetHealthAndStuff();
         }
-        if(lose) {
-            for(Hero hero : Dank.allHeroesInGameList) {
+        if (lose) {
+            for (Hero hero : Dank.allHeroesInGameList) {
                 hero.healthCurrent = hero.healthTotal;
             }
             Image loseImage = new Image(new Texture(Gdx.files.internal("inne/defeat.png")));
@@ -425,12 +458,12 @@ public class DankGame extends BasicScreen {
 
     private void artificialIntelligence() {
         int i;
-        for( i=0;;i++) {
-            if(i==4) {
+        for (i = 0; ; i++) {
+            if (i == 4) {
                 checkIfGameEnded();
                 return;
             }
-            if(Dank.chosenHeroesList.get(i).healthCurrent>0){
+            if (Dank.chosenHeroesList.get(i).healthCurrent > 0) {
                 break;
             }
         }
@@ -468,7 +501,7 @@ public class DankGame extends BasicScreen {
                 return -2; //Error
             }
         }));
-        if(Dank.thisTurnLeftHeroesList.isEmpty()) {
+        if (Dank.thisTurnLeftHeroesList.isEmpty()) {
             Dank.thisTurnLeftHeroesList.addAll(Dank.allHeroesInGameList);
         }
         Dank.activeHero = Dank.thisTurnLeftHeroesList.get(0);
