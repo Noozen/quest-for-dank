2f605f9c05ac690f24de5504799e8e14ae0a5d32
Mikolaj <mikolaj.nalecz@hotmail.com>
2015-09-06 11:17:03 +0200

    Dank MainMenu added.

--- core/src/com/dankquest/game/com/dankquest/game/screens/MainMenu.java
+++ core/src/com/dankquest/game/com/dankquest/game/screens/MainMenu.java
@@ -9,17 +9,21 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class MainMenu extends BasicScreen {
 
-    private int currentHero;
+    private float timer = 0;
+    private int cameraCountdown = 0;
 
     private Stage stage;
     private Table table;
@@ -45,6 +49,10 @@ public class MainMenu extends BasicScreen {
     @Override
     public void show() {
         stage = new Stage(new FitViewport(640,480));
+        stage.getCamera().far = 500f;
+        stage.getCamera().near = 10f;
+        stage.getCamera().position.set(320, 240, 100);
+        stage.getCamera().lookAt(320, 240, 0);
         Gdx.input.setInputProcessor(stage);
 
         table = new Table();
@@ -52,12 +60,16 @@ public class MainMenu extends BasicScreen {
         stage.addActor(table);
 
         //Music Setup
-        relaxingMusic = Gdx.audio.newMusic(Gdx.files.internal("music/relaxing_music.mp3"));
+        relaxingMusic = Gdx.audio.newMusic(Gdx.files.internal("music/cool_music.mp3"));
         relaxingMusic.setVolume(0.2f);
         relaxingMusic.setLooping(true);
         relaxingMusic.play();
+
         //Background Setup
         backgroundImage = new Image(new TextureRegion(new Texture(Gdx.files.internal("main_menu/main_menu_background.png"))));
+        backgroundImage.setX(-80);
+        backgroundImage.setY(-60);
+
         table.addActor(backgroundImage);
 
         //Font Setup
@@ -73,8 +85,8 @@ public class MainMenu extends BasicScreen {
         //Play Button Setup
         playButton = new TextButton("PLAY", buttonStyle);
 
-        playButton.setX(75);
-        playButton.setY(Gdx.graphics.getHeight() * 3 / 4 - playButton.getHeight()/2);
+        playButton.setX(370);
+        playButton.setY(Gdx.graphics.getHeight() * 3 / 4 - playButton.getHeight()/2 - 30);
 
         playButton.addListener(new InputListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -96,8 +108,8 @@ public class MainMenu extends BasicScreen {
         //Settings Button Setup
         settingsButton = new TextButton("OPTIONS", buttonStyle);
 
-        settingsButton.setX(20);
-        settingsButton.setY(Gdx.graphics.getHeight() * 2 / 4 - playButton.getHeight()/2);
+        settingsButton.setX(320);
+        settingsButton.setY(Gdx.graphics.getHeight() * 2 / 4 - playButton.getHeight()/2 - 30);
 
         settingsButton.addListener(new InputListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -119,8 +131,8 @@ public class MainMenu extends BasicScreen {
         //Exit Button Setup
         exitButton = new TextButton("EXIT", buttonStyle);
 
-        exitButton.setX(75);
-        exitButton.setY(Gdx.graphics.getHeight() / 4 - playButton.getHeight()/2);
+        exitButton.setX(370);
+        exitButton.setY(Gdx.graphics.getHeight() / 4 - playButton.getHeight()/2 - 30);
 
         exitButton.addListener(new InputListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -134,6 +146,29 @@ public class MainMenu extends BasicScreen {
 
     @Override
     public void render(float delta) {
+        timer+=delta;
+        if(timer>=0.46875f) {
+            timer -= 0.46875f;
+            cameraCountdown = 20;
+        }
+        if(cameraCountdown>0) {
+            if(cameraCountdown>4) {
+                stage.getCamera().position.y+=1;
+                stage.getCamera().lookAt(320,240,0);
+                stage.getCamera().update();
+            }
+            if(cameraCountdown<=4) {
+                stage.getCamera().position.y-=4;
+                stage.getCamera().lookAt(320,240,0);
+                stage.getCamera().update();
+            }
+            if(cameraCountdown == 1) {
+                stage.getCamera().lookAt(320, 240, 0);
+                stage.getCamera().up.set(0,1,0);
+                stage.getCamera().update();
+            }
+            cameraCountdown--;
+        }
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.draw();
