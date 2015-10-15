ef39143ce9356c4022cac8cbb6c79ba01fd63981
Mikolaj <mikolaj.nalecz@hotmail.com>
2015-10-15 16:02:09 +0200

    Updated TODO.

--- core/src/com/dankquest/game/com/dankquest/game/logic/skill/IcelanceSkill.java
+++ core/src/com/dankquest/game/com/dankquest/game/logic/skill/IcelanceSkill.java
@@ -17,7 +17,8 @@ public class IcelanceSkill extends Skill {
     private Animation skillAnimationBreak, skillAnimationFlying, skillAnimationCreate;
     protected TextureRegion[] skillFramesBreak, skillFramesFlying, skillFramesCreate;
     private float flyingAnimationTime;
-    public IcelanceSkill(){
+
+    public IcelanceSkill() {
         amountOfTargets = 1;
         basePixmap = new Pixmap(Gdx.files.internal("skills/arrow.png"));
         skillName = "Ice Lance";
@@ -25,11 +26,11 @@ public class IcelanceSkill extends Skill {
         TextureRegion[][] tmp = TextureRegion.split(skillSheet, 192, 192);
         skillFramesCreate = new TextureRegion[8];
         int index = 0, i;
-        for(i = 0; i < 5; i++){
+        for (i = 0; i < 5; i++) {
             skillFramesCreate[index] = tmp[0][i];
             index++;
         }
-        for(i = 1; i < 4; i++){
+        for (i = 1; i < 4; i++) {
             skillFramesCreate[index] = tmp[1][i];
             index++;
         }
@@ -38,18 +39,20 @@ public class IcelanceSkill extends Skill {
         skillFramesBreak = new TextureRegion[4];
         index = 1;
         skillFramesBreak[0] = tmp[1][4];
-        for(i = 0; i < 3; i++){
+        for (i = 0; i < 3; i++) {
             skillFramesBreak[index] = tmp[2][i];
             index++;
         }
         skillAnimationCreate = new Animation(0.05f, skillFramesCreate);
         skillAnimationBreak = new Animation(0.1f, skillFramesBreak);
-}
+    }
 
     public void cast() {
-        double damage = Dank.targetList.get(0).getAttackDamage()*2 * Dank.targetList.get(0).getDamageMultiplier();
+        double damage = Dank.targetList.get(0).getAttackDamage() * 2 * Dank.targetList.get(0).getDamageMultiplier();
         Dank.targetList.get(1).healthCurrent -= damage;
-    };
+    }
+
+    ;
 
     @Override
     public void draw(Batch batch, float playTime) {
@@ -71,34 +74,34 @@ public class IcelanceSkill extends Skill {
         }
     }
 
-@Override
-public float getX() {
-        return Dank.targetList.get(1).heroActor.getX()-64;
-        }
+    @Override
+    public float getX() {
+        return Dank.targetList.get(1).heroActor.getX() - 64;
+    }
 
-@Override
-public float getY() {
-        return Dank.targetList.get(1).heroActor.getY()-64;
-        }
+    @Override
+    public float getY() {
+        return Dank.targetList.get(1).heroActor.getY() - 64;
+    }
 
     @Override
     public void createAnimation() {
         double x = Dank.targetList.get(1).heroActor.getX() - Dank.targetList.get(0).heroActor.getX();
         double y = Dank.targetList.get(1).heroActor.getY() - Dank.targetList.get(0).heroActor.getY();
-        double pathLength = sqrt(Math.pow(x,2) + Math.pow(y,2));
-        flyingAnimationTime = (float)pathLength/600;
+        double pathLength = sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        flyingAnimationTime = (float) pathLength / 600;
         skillAnimationFlying = new Animation(flyingAnimationTime, skillFramesFlying);
     }
 
     @Override
     public float getAnimationDuration() {
-        return skillAnimationBreak.getAnimationDuration()+skillAnimationFlying.getAnimationDuration()+ skillAnimationCreate.getAnimationDuration();
+        return skillAnimationBreak.getAnimationDuration() + skillAnimationFlying.getAnimationDuration() + skillAnimationCreate.getAnimationDuration();
     }
 
     @Override
     public boolean isAnimationFinished(float playTime) {
         playTime -= skillAnimationCreate.getAnimationDuration() + skillAnimationFlying.getAnimationDuration();
-        if(playTime < 0)
+        if (playTime < 0)
             playTime = 0;
         return skillAnimationBreak.isAnimationFinished(playTime);
     }
